31978e553a250e0191791a7a5e27159b9e900ddc
이소희 <shfksthgml@naver.com>
2021-05-04 22:26:46 +0900

    feat: Add product dao file

---
+++ HW_SF_05_RestAPI/src/main/java/com/ssafy/product/model/mapper/ProductDao.java
@@ -0,0 +1,20 @@
+package com.ssafy.product.model.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.ssafy.product.model.dto.Product;
+
+public interface ProductDao {
+	// CRUD
+
+	List<Product> selectAll() throws SQLException;
+
+	Product select(String id) throws SQLException;
+
+	int insert(Product product) throws SQLException;
+
+	int update(Product product) throws SQLException;
+
+	int delete(String id) throws SQLException;
+}
