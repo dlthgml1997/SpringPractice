8340448bd59544e3d57644d30eaeaa628035dc08
이소희 <shfksthgml@naver.com>
2021-05-04 22:37:41 +0900

    feat: Add enableaspectjautoproxy annotation for aop

---
+++ HW_SF_06_SpringBoot/src/main/java/com/ssafy/product/HwSf06SpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.ssafy.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class HwSf06SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSf06SpringBootApplication.class, args);
+	}
+
+}
